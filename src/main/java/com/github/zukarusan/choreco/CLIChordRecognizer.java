a574707251cf8067ee623bff830ef004f5c5af4d
zukaru <llamanya1207>
2022-01-23 21:27:07 +0700

    add comment for demo

--- src/main/java/com/github/zukarusan/choreco/CLIChordRecognizer.java
+++ src/main/java/com/github/zukarusan/choreco/CLIChordRecognizer.java
@@ -19,23 +19,25 @@ public class CLIChordRecognizer {
         ChordProcessor.isDebug = false;
         AudioDispatcher dispatcher = AudioDispatcherFactory.fromDefaultMicrophone((int) SAMPLE_RATE, BUFFER_SIZE, BUFFER_SIZE/2);
         PrintStream output_chords = new PrintStream(System.out);
-        try(ChordProcessor chordProcessor = new ChordProcessor(SAMPLE_RATE, BUFFER_SIZE, output_chords)) {
-            dispatcher.addAudioProcessor(chordProcessor);
+        ChordProcessor chordProcessor = new ChordProcessor(SAMPLE_RATE, BUFFER_SIZE, output_chords);
+        dispatcher.addAudioProcessor(chordProcessor);
+        Runtime.getRuntime().addShutdownHook(new Thread(() -> {  // SAFE SHUTDOWN HOOK FOR RELEASING RESOURCES
+                System.out.println("Releasing Resources");
+                chordProcessor.close();
+        }));
+        chordThread = new Thread(dispatcher, "Chord recognizer");
 
-            Runtime.getRuntime().addShutdownHook(new Thread(() -> {
-                    System.out.println("Releasing Resources");
-                    chordProcessor.close();
-            }));
-            chordThread = new Thread(dispatcher, "Chord recognizer");
+        System.out.println("============================ CHORD RECOGNIZER ========================================");
+        System.out.println("               Input chars and enter to exit (interrupt) the program                  ");
+        System.out.println("======================================================================================");
+        chordThread.start();
+        new BufferedReader(new InputStreamReader(System.in)).readLine();
+        chordThread.interrupt();
 
-            System.out.println("============================ CHORD RECOGNIZER ========================================");
-            System.out.println("               Input chars and enter to exit (interrupt) the program                  ");
-            System.out.println("======================================================================================");
-            chordThread.start();
-            new BufferedReader(new InputStreamReader(System.in)).readLine();
-            chordThread.interrupt();
-            dispatcher.stop();
-            System.exit(0);
-        }
+        dispatcher.stop(); // This will call the close() in the chord processor class
+
+        chordProcessor.close(); // Again, just a safe method call to close the ChordProcessor to
+                                // release the resources in tensorflow model and output stream
+        System.exit(0);
     }
 }
